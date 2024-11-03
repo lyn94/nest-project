50faba1fde315ce68bfa8f6dd202dd5643528f42
yunalee <lyn_94@naver.com>
2024-11-03 19:20:00 +0900

    1.1 컨트롤러 생성해보기

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,10 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get("/hello") // 데코레이터는 함수나 클래스와 붙여서 써야한다.
+  sayHello(): string {
+    return 'Hello everyone'
+  }
+
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController], // express.js의 라우터 같은 역할, url을 가져오고 함수를 실행
   providers: [AppService],
 })
 export class AppModule {}
