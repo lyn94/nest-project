81315cc1c6002af959f125ed7152e931db70b52b
Yu-Na Lee <43225974+lyn94@users.noreply.github.com>
2024-11-05 23:44:34 +0900

    2.1 컨트롤러의 파라미터, 쿼리, 바디로 요청 받기

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query } from '@nestjs/common';
 
 @Controller('movies') //entry point
 export class MoviesController {
@@ -8,25 +8,32 @@ export class MoviesController {
         return 'This will return all movies'
     }
 
-    @Get("/:id")
+    @Get("search") // 파라미터 받는 것보다 아래에 코드가 위치하면 search를 :id로 인식한다. (express.js도 동일)
+    search(@Query("year") searchingYear: string){
+        return `We are searching for a movie made after: ${searchingYear}`
+    }
+
+    @Get(":id")
     getOne(@Param('id') movieId: string){ // 파라미터 값 받기
         return `This will return one movie with the id: ${movieId}`
     }
 
     @Post()
-    create() {
-        return 'This will create a movie'
+    create(@Body() movieData) {
+        return movieData
     }
 
-    @Delete("/:id")
+    @Delete(":id")
     remove(@Param('id') movieId: string){
         return `This will delete a movie with the id: ${movieId}`
     }
 
     // @Put: 전부다 수정, @Patch: 일부만 수정
-    @Patch('/:id')
-    patch(@Param('id') movieId: string) {
-        return `This will patch a movie with the id: ${movieId}`
+    @Patch(':id')
+    patch(@Param('id') movieId: string, @Body() updateData) {
+        return {
+            updateMovie: movieId,
+            ...updateData,
+        }
     }
-
 }
