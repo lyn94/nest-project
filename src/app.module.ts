04c3158264c6cab2360d7ec1ad8506ca12f67c3d
Yu-Na Lee <43225974+lyn94@users.noreply.github.com>
2024-11-07 00:23:07 +0900

    2.2 서비스를 생성하고 기존 컨트롤러와 연결하기

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MoviesController } from './movies/movies.controller';
+import { MoviesService } from './movies/movies.service';
 
 @Module({
   imports: [],
   controllers: [MoviesController], // controller: express.js의 라우터 같은 역할, url을 가져오고 함수를 실행
-  providers: [], // service: 함수들이 있는 곳, 비즈니스 로직
+  providers: [MoviesService], // service: 함수들이 있는 곳, 비즈니스 로직
 })
 export class AppModule {}
--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,31 +1,35 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query } from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie.entity';
 
 @Controller('movies') //entry point
 export class MoviesController {
 
-    @Get()
-    getAll(){
-        return 'This will return all movies'
-    }
+    constructor(readonly moviesService: MoviesService) {}
 
+    @Get()
+    getAll(): Movie[]{
+        return this.moviesService.getAll();
+    }
+/*
     @Get("search") // 파라미터 받는 것보다 아래에 코드가 위치하면 search를 :id로 인식한다. (express.js도 동일)
     search(@Query("year") searchingYear: string){
         return `We are searching for a movie made after: ${searchingYear}`
     }
-
+*/
     @Get(":id")
-    getOne(@Param('id') movieId: string){ // 파라미터 값 받기
-        return `This will return one movie with the id: ${movieId}`
+    getOne(@Param('id') movieId: string): Movie{ // 파라미터 값 받기
+        return this.moviesService.getOne(movieId);
     }
 
     @Post()
     create(@Body() movieData) {
-        return movieData
+        return this.moviesService.create(movieData);
     }
 
     @Delete(":id")
     remove(@Param('id') movieId: string){
-        return `This will delete a movie with the id: ${movieId}`
+        return this.moviesService.deleteOne(movieId);
     }
 
     // @Put: 전부다 수정, @Patch: 일부만 수정
