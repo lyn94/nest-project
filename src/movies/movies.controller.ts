692902db7850d5afe4beef6ca86245d9f730f747
Yu-Na Lee <43225974+lyn94@users.noreply.github.com>
2025-01-31 19:11:50 +0900

    chroe: eslint 수정

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -4,37 +4,37 @@ import { Movie } from './entities/movie.entity';
 
 @Controller('movies') //entry point
 export class MoviesController {
+  constructor(readonly moviesService: MoviesService) {}
 
-    constructor(readonly moviesService: MoviesService) {}
-
-    @Get()
-    getAll(): Movie[]{
-        return this.moviesService.getAll();
-    }
-/*
+  @Get()
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
+  }
+  /*
     @Get("search") // 파라미터 받는 것보다 아래에 코드가 위치하면 search를 :id로 인식한다. (express.js도 동일)
     search(@Query("year") searchingYear: string){
         return `We are searching for a movie made after: ${searchingYear}`
     }
 */
-    @Get(":id")
-    getOne(@Param('id') movieId: string): Movie{ // 파라미터 값 받기
-        return this.moviesService.getOne(movieId);
-    }
+  @Get(':id')
+  getOne(@Param('id') movieId: string): Movie {
+    // 파라미터 값 받기
+    return this.moviesService.getOne(movieId);
+  }
 
-    @Post()
-    create(@Body() movieData) {
-        return this.moviesService.create(movieData);
-    }
+  @Post()
+  create(@Body() movieData) {
+    return this.moviesService.create(movieData);
+  }
 
-    @Delete(":id")
-    remove(@Param('id') movieId: string){
-        return this.moviesService.deleteOne(movieId);
-    }
+  @Delete(':id')
+  remove(@Param('id') movieId: string) {
+    return this.moviesService.deleteOne(movieId);
+  }
 
-    // @Put: 전부다 수정, @Patch: 일부만 수정
-    @Patch(':id')
-    patch(@Param('id') movieId: string, @Body() updateData) {
-        return this.moviesService.update(movieId, updateData);
-    }
+  // @Put: 전부다 수정, @Patch: 일부만 수정
+  @Patch(':id')
+  patch(@Param('id') movieId: string, @Body() updateData) {
+    return this.moviesService.update(movieId, updateData);
+  }
 }
